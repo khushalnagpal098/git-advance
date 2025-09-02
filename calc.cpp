6dbbd232dc1ec64c853ca1bf2929fadb6b11fe39
Khushal Nagpal <khushalnagpal098@gmail.com>
2025-09-02 16:47:46 +0530

    menu driven in calc

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int choice;
+    cout<<"0. Exit "<<endl;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Subtration"<<endl;
+    cout<<"3. Multiplication"<<endl;
     
     return 0;
 }
