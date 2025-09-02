e4517fc9ddd2b0165894385c957721b40295123a
Khushal Nagpal <khushalnagpal098@gmail.com>
2025-09-02 16:15:53 +0530

    some changes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int choice;
+    cout<<"1. Addition"<<endl;
+    cout<<"2. Subtraction"<<endl;
+    cout<<"3. Multiplication"<<endl;
+    cout<<"4. Division"<<endl;
+    
     return 0;
 }
