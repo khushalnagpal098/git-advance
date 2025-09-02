85f27328f0b35ec3dcd80d37c807f438c99466c2
Khushal Nagpal <khushalnagpal098@gmail.com>
2025-09-02 16:29:36 +0530

    menudriven program

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 int main(){
     int choice;
+    cout<<"0. Exit"<<endl;
     cout<<"1. Addition"<<endl;
     cout<<"2. Subtraction"<<endl;
     cout<<"3. Multiplication"<<endl;
     cout<<"4. Division"<<endl;
+    cout<<"Enter Your Choice : "<<endl;
+    cin>>choice;
+    while (choice != 0)
+    {
+        switch (choice)
+        {
+        case 1:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
+    }
+    
     
     return 0;
 }
